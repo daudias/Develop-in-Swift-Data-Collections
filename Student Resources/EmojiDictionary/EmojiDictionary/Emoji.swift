de6def9b8de471dd4f03bd2c29afc4a62fd46477
Dias <47509783+daudias@users.noreply.github.com>
2020-12-13 03:35:59 +0600

    done lab 7

---
+++ Student Resources/EmojiDictionary/EmojiDictionary/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Dias on 12/12/20.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
